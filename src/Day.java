d34f10045f81e038e735be5d160932634852bdd7
laviealon <79278018+laviealon@users.noreply.github.com>
2021-10-14 11:57:46 -0400

    Changed the consturctor in class Day.

--- src/Day.java
+++ src/Day.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 /**
  * Each day should comprise its date, day, and a map with half-hour intervals as keys and task names as values.
+ * The empty string as a value represents no task being scheduled, i.e. free time.
  */
 
 public class Day {
@@ -15,7 +16,12 @@ public class Day {
     public Day(LocalDate dayOfMonth){
         this.dayOfMonth = dayOfMonth;
         this.dayOfWeek = dayOfMonth.getDayOfWeek();
-        this.todaySchedule = new HashMap<Double, String>();
+        this.todaySchedule = new HashMap<>();
+        // Set all initial half-hour blocks to the empty string, which represents
+        // no task scheduled (free time)/
+        for(double i = 0.0; i < 24.00; i = i + 0.5){
+            this.todaySchedule.put(i, "");
+        }
     }
 
     // TODO: account for free time before first task and after last task.
