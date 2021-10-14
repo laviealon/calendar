6089422e5623a15d6e3a2927d6eda96ed2e79142
Runyu Xia <runyu.xia@mail.utoronto.ca>
2021-10-14 10:28:47 -0400

    project modify

--- src/Project.java
+++ src/Project.java
@@ -2,29 +2,11 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
 public class Project {
-    /** Why do we need the week it is scheduling onto?
-     * Do we want to return a double or round it up?
-     */
-    public String name;
-    public double totalHours;
-    public double frequency;
-    public LocalDate startDate;
-    public LocalDate dueDate;
 
-    // user interface or controller convert user input into LocalDate object
-
-    public Project(String name, double totalHours, double frequency, LocalDate startDate,
-                   LocalDate dueDate){
-
-        this.name = name;
-        this.totalHours = totalHours;
-        this.frequency = frequency;
-        this.startDate = startDate;
-        this.dueDate = dueDate;
+    public double calculateMinHours(LocalDate startDate, LocalDate dueDate, double totalHours, double frequency) {
+        long diff = ChronoUnit.DAYS.between(startDate, dueDate);
+        double idealChunk = totalHours/(diff*(frequency/7));
+        return Math.max(idealChunk, 0.5);
     }
 
-    public double calculateMinHours() {
-        long diff = ChronoUnit.DAYS.between(this.startDate, this.dueDate);
-        return this.totalHours/diff;
-    }
 }
