02fa90dab5f97224a18b2a1bd44299016f74cac1
laviealon <79278018+laviealon@users.noreply.github.com>
2021-10-14 11:04:37 -0400

    Create placeholder return statements so that our code can run.

--- src/FixedTask.java
+++ src/FixedTask.java
@@ -2,7 +2,7 @@ import java.time.LocalDateTime;
 
 public class FixedTask extends Task {
 
-    public FixedTask(String name, LocalDateTime startTime,  double duration){
+    public FixedTask(String name, double startTime,  double duration){
         super(name, startTime, duration);
     }
 
