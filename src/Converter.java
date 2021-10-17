b9f8ea2d391121ddc29345baf5d38921abcaa45b
Runyu Xia <runyu.xia@mail.utoronto.ca>
2021-10-16 21:24:04 -0400

    converter and putter modified

---
+++ src/Converter.java
@@ -0,0 +1,26 @@
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+
+public class Converter {
+
+    public double ConvertDateTimeDouble (LocalDateTime localTime){
+        int hour = localTime.getHour();
+        int minute = localTime.getMinute();
+        double minuteDouble = ((double)minute/60);
+        return (double)hour + minuteDouble;}
+
+    public double ConvertLocalTimeDouble (LocalTime localTime){
+        int hour = localTime.getHour();
+        int minute = localTime.getMinute();
+        double minuteDouble = ((double)minute/60);
+        return (double)hour + minuteDouble;}
+
+
+    public LocalTime ConvertDoubleToLocalTime (Double time){
+        int hour = (int)Math.floor(time);
+        int minute = (int)(time - hour)*60;
+        return LocalTime.of(hour, minute);
+    }
+
+}
--- src/Putter.java
+++ src/Putter.java
@@ -3,18 +3,38 @@ import java.time.LocalDateTime;
 public class Putter {
 
     // TODO: REQUIRED FOR WALKTHROUGH - DO NOT NEED TO HANDLE CASE WHERE TASK OVERLAPS DAY.
+
+
+
     public static void putTask(Week week, Task task){
         // loop through half-hours and put them into the correct day using Day.putHalfHourTask.
         LocalDateTime taskStart = task.startDateTime;
         for (Day day: week.days) {
             // find the day this task starts in this week
             if (taskStart.toLocalDate().isEqual(day.dayOfMonth)) {
+                //convert task duration to double
+                int hour = task.duration.getHour();
+                int minute = task.duration.getMinute();
+                double minuteDouble = ((double)minute/60);
+                //calculate the number of half hour block represented by the minute
+                double minuteBlock = minuteDouble/0.5;
+                //calculate total number of half hour blocks in the task duration
+                int totalTaskBlock = hour*2 + (int) minuteBlock;
+                //convert the start time of task to double
+                double h = task.startDateTime.getHour();
+                double m = (double)(task.startDateTime.getMinute())/60;
+                double startTimeD = h + m;
+                //loop through todaySchedule and mutate the value of the corresponding
+                int i = 0;
+                while (i < totalTaskBlock) {
+                    day.todaySchedule.replace(startTimeD + 0.5*i, task.name);
+                    i ++;
+                }
                 // find the start time of the task in the day
-
             }
-
         }
     }
+
 
 
     /**
@@ -23,16 +43,20 @@ public class Putter {
      * distinct half-hour intervals, and (2) if a task crosses over multiple days,
      * different half-hours will need to be scheduled onto different days.
      *
-     * @param taskName: the name of the task which the hour that is to be scheduled is in.
-     * @param startHour: the half-hour that marks the beginning of this hour of a task.
+     *taskName: the name of the task which the hour that is to be scheduled is in.
+     * startHour: the half-hour that marks the beginning of this hour of a task.
      *                 Must be between 0 and 23.5.
      */
-    protected void putHalfHourTask(Week week, Day day, Double startHour, String taskName){
-        day.todaySchedule.put(startHour, taskName);
-    }
+    //protected void putHalfHourTask(Week week, Day day, Double startHour, String taskName){
+        //day.todaySchedule.put(startHour, taskName);
+    //}
 
     // TODO: REQUIRED FOR WALKTHROUGH.
     public static void putProject(String projectName, Week week, NonFixedTask[] tasks){
+        for (NonFixedTask task : tasks) {
+            task.name = projectName;
+            putTask(week, task);
+        }
 
     }
 }
