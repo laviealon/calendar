6d2307bd5adf56108909b26376755764d89325e1
thelf <thelfenbaum@gmail.com>
2021-10-16 23:28:58 -0400

    editied doctstrings for controller

--- src/Controller.java
+++ src/Controller.java
@@ -4,11 +4,12 @@ public class Controller {
 
 
 
+    // instantiate a week object to be populated by input from UI
     public static Week activateInstantiateWeek(LocalDate startDate){
         return InstantiateWeek.instantiateWeek(startDate);
     }
 
-
+    // initiates scheduling and putting of fixed task if user chooses to add one to their schedule in UI
     public static void activateFixedTaskScheduling(Week week, FixedTask taskToPut){
         Putter.putTask(week, taskToPut);
     }
@@ -18,6 +19,7 @@ public class Controller {
         Putter.putTask(week, NonFixedTaskToPut);
     }
 
+    // initiate shceduling and putting of project based on user preferences
     public static void activateProjectScheduling(Week week, NonFixedTask[] projectTasksToSchedule){
 //
         NonFixedTask[] projectTasksToPut = Scheduler.ScheduleProject(week, projectTasksToSchedule);
